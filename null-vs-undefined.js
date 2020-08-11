2d313b4b9c2df771a426b1050016e0d5f3bbd9a0
Suresh Chakma <34184573+sureshchakma@users.noreply.github.com>
2020-08-11 10:20:26 +0600

    I love programming

---
+++ null-vs-undefined.js
@@ -0,0 +1,20 @@
+let pakhi;
+function add(num1, num2){
+    console.log(num1 + num2);
+return
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+
+const premik= {name: 'Suresh Chakma', age: 25, wife: 'Boishakhi Chakma'};
+console.log(premik.wife);
+
+let fun = undefined;
+console.log(fun);
+
+let ages= [2, 42, 3];
+console.log(ages[3]);
+
